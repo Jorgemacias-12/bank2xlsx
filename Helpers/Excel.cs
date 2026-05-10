0e91ed1bab112d400092f518d65da38ae5c809c0
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-10 14:58:33 -0600

    add: Excel.cs & Number.cs
    In the Helpers folder

---
+++ Helpers/Excel.cs
@@ -0,0 +1,67 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank2Pdf.Helpers
+{
+    internal class Excel
+    {
+        public static Row CreateRow(
+            List<string> values,
+            int rowIndex)
+        {
+            Row row = new()
+            {
+                RowIndex = (uint)rowIndex
+            };
+
+            for (int columnIndex = 0;
+                 columnIndex < values.Count;
+                 columnIndex++)
+            {
+                string cellReference =
+                    GetCellReference(columnIndex + 1, rowIndex);
+
+                Cell cell = new()
+                {
+                    CellReference = cellReference,
+                    DataType = CellValues.String,
+                    CellValue = new CellValue(values[columnIndex] ?? string.Empty)
+                };
+
+                row.Append(cell);
+            }
+
+            return row;
+        }
+
+        private static string GetCellReference(
+             int columnNumber,
+             int rowNumber)
+        {
+            const int alphabetLength = 26;
+            const int asciiUppercaseA = 65;
+            const int excelColumnOffset = 1;
+
+            string columnName = string.Empty;
+
+            while (columnNumber > 0)
+            {
+                int remainder =
+                    (columnNumber - excelColumnOffset)
+                    % alphabetLength;
+
+                columnName =
+                    (char)(asciiUppercaseA + remainder)
+                    + columnName;
+
+                columnNumber =
+                    (columnNumber - remainder - excelColumnOffset)
+                    / alphabetLength;
+            }
+
+            return $"{columnName}{rowNumber}";
+        }
+    }
+}
---
+++ Helpers/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank2Pdf.Helpers
+{
+    internal class Number
+    {
+        public static bool IsNumeric(string value)
+        {
+            value = value.Replace(",", "");
+
+            return decimal.TryParse(value, out _);
+        }
+    }
+}
