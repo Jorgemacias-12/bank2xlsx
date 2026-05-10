60c40bf11d77f223eb7b66028bdcd2795e5509d3
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:29:21 -0600

    add: Components / FileList
    Created and ready to show the following propperties:
    
     - name
     - fullpath
     - size (long value)
     - formattedsize with a helper function

---
+++ Components/FileList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bank2Pdf.Components
+{
+    /// <summary>
+    /// Lógica de interacción para FileList.xaml
+    /// </summary>
+    public partial class FileList : UserControl
+    {
+        public FileList()
+        {
+            InitializeComponent();
+        }
+    }
+}
