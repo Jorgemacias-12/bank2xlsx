ec14536feb3d0a80ff95120811da43e6c3d14673
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:25:29 -0600

    add: FileParser service
    TODO: only adding file with a TODO: notice nothing implemented yet!!!

---
+++ Services/FileParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Bank2Pdf.Services
+{
+    class FileParser
+    {
+        public async Task ParseAsync(string path)
+        {
+            var lines = await File.ReadAllLinesAsync(path);
+
+            // TODO: implement parsing logic later
+        }
+    }
+}
