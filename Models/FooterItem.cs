d40aa5ed30a8193311cc9444b816a5a7d3757bf3
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-10 14:59:22 -0600

    add: FooterItem.cs & ParsedFile.cs
    In the models folder add these files used by the parser of the txt file
    and the exporter to excel xlsx file.

---
+++ Models/FooterItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank2Pdf.Models
+{
+    public class FooterItem
+    {
+        public string Key { get; set; } = "";
+
+        public string Value { get; set; } = "";
+
+        public bool IsCurrency { get; set; }
+    }
+}
---
+++ Models/ParsedFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank2Pdf.Models
+{
+    public class ParsedFile
+    {
+        public DroppedFile? SourceFile { get; set; }
+
+        public List<string> Headers { get; set; } = [];
+
+        public List<List<string>> Rows { get; set; } = [];
+
+        public List<FooterItem> Footer { get; set; } = [];
+    }
+}
