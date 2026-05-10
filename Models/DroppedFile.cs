ca17e9df33181c568bf44c9c6da7c3506185d5bf
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:27:42 -0600

    add: DroppedFile dto
    With MVVM properties from the package MVVM community created by microsft
    to making MVVM no more pain in the ass C:

---
+++ Models/DroppedFile.cs
@@ -0,0 +1,26 @@
+﻿using Bank2Pdf.Helpers;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank2Pdf.Models
+{
+    partial class DroppedFile : ObservableObject
+    {
+        [ObservableProperty]
+        private string name = "";
+
+        [ObservableProperty]
+        private string fullPath = "";
+
+        [ObservableProperty]
+        private long size;
+
+        [ObservableProperty]
+        private string status = "Pendiente";
+
+        public string FormattedSize =>
+            FileSizeFormatter.Format(Size);
+    }
+}
