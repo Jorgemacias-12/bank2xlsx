69beb5348dba1a22a0ca703736faf73410087bb6
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:30:26 -0600

    add: Helpers/FileSizeFormatter.cs
    Ah yes, no more 1024859 bytes.

---
+++ Helpers/FileSizeFormatter.cs
@@ -0,0 +1,36 @@
+﻿namespace Bank2Pdf.Helpers;
+
+public static class FileSizeFormatter
+{
+    private const int Scale = 1024;
+
+    private const string BytesSuffix = "B";
+    private const string KilobytesSuffix = "KB";
+    private const string MegabytesSuffix = "MB";
+    private const string GigabytesSuffix = "GB";
+    private const string TerabytesSuffix = "TB";
+
+    private static readonly string[] SizeSuffixes =
+    [
+        BytesSuffix,
+        KilobytesSuffix,
+        MegabytesSuffix,
+        GigabytesSuffix,
+        TerabytesSuffix
+    ];
+
+    public static string Format(long bytes)
+    {
+        double size = bytes;
+        int suffixIndex = 0;
+
+        while (size >= Scale &&
+               suffixIndex < SizeSuffixes.Length - 1)
+        {
+            suffixIndex++;
+            size /= Scale;
+        }
+
+        return $"{size:0.##} {SizeSuffixes[suffixIndex]}";
+    }
+}
