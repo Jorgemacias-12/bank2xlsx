d3118612c351ab65ae9f410d7b828bc6ff6999a8
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:23:06 -0600

    add: ViewModels/MainViewModel.cs
    Create MainViewModel with the final structure that works, allowing to
    drag and drop files and then parse them as an excel file depending of
    the file contents.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,98 @@
+﻿using Bank2Pdf.Models;
+using Bank2Pdf.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+
+namespace Bank2Pdf.ViewModels
+{
+    partial class MainViewModel : ObservableObject
+    {
+        private readonly FileParser _parserService;
+
+        public ObservableCollection<DroppedFile> Files { get; } = new();
+
+        public MainViewModel()
+        {
+            _parserService = new FileParser();
+        }
+
+        [RelayCommand]
+        private void DragOver(DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.None;
+                e.Handled = true;
+
+                return;
+            }
+
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            bool hasTxt = files.Any(file =>
+                Path.GetExtension(file)
+                    .Equals(".txt", StringComparison.OrdinalIgnoreCase));
+
+            e.Effects = hasTxt
+                ? DragDropEffects.Copy
+                : DragDropEffects.None;
+
+            e.Handled = true;
+        }
+
+        [RelayCommand]
+        private void Drop(DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            HandleDrop(files);
+        }
+
+        public void HandleDrop(string[] files)
+        {
+            var txtFiles = files
+                .Where(static f => Path.GetExtension(f)
+                .Equals(".txt", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var file in txtFiles)
+            {
+                var info = new FileInfo(file);
+
+                Files.Add(new DroppedFile
+                {
+                    Name = info.Name,
+                    FullPath = info.FullName,
+                    Size = info.Length
+                });
+            }
+        }
+
+        [RelayCommand]
+        private async Task ParseFileAsync(DroppedFile file)
+        {
+            if (file is null)
+                return;
+
+            MessageBox.Show("");
+
+            file.Status = "Convirtiendo...";
+
+            try
+            {
+                await _parserService.ParseAsync(file.FullPath);
+
+                file.Status = "Completado";
+            }
+            catch
+            {
+                file.Status = "Error al convertir contacte con el creador";
+            }
+        }
+    }
+}
