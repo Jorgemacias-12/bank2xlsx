9fcccfbe665b05e1578c90e5c75b78ee8b7fcdc0
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-09 21:16:47 -0600

    update: App.xaml.cs
    Init window here instead of using App.xaml startup uri, overwriting the
    OnStartup method.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using Bank2Pdf.ViewModels;
+using Bank2Pdf.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +12,24 @@ namespace Bank2Pdf
     /// </summary>
     public partial class App : Application
     {
+        public static ServiceProvider? Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton<MainViewModel>();
+            serviceCollection.AddSingleton<MainWindow>();
+
+            Services = serviceCollection.BuildServiceProvider();
+
+            var window = Services.GetRequiredService<MainWindow>();
+
+            window.DataContext =
+                Services.GetRequiredService<MainViewModel>();
+
+            window.Show();
+        }
     }
 
 }
