cb2c587be81eeb936bb79fc0a9bdf6422ce81f94
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-10 10:29:47 -0600

    add: ExcelExporter service

---
+++ Services/Exporters/ExcelExporter.cs
@@ -0,0 +1,147 @@
+﻿using Bank2Pdf.Models;
+using ClosedXML.Excel;
+
+namespace Bank2Pdf.Services.Exporters;
+
+public class ExcelExporter
+{
+    public XLWorkbook CreateWorkbook(ParsedFile parsedFile)
+    {
+        XLWorkbook workbook = new();
+
+        IXLWorksheet worksheet =
+            CreateWorksheet(workbook);
+
+        InsertHeaders(
+            worksheet,
+            parsedFile.Headers);
+
+        InsertRows(
+            worksheet,
+            parsedFile.Rows);
+
+        InsertFooter(
+            worksheet,
+            parsedFile.Footer
+        );
+
+        CreateTable(
+            worksheet,
+            parsedFile.Headers.Count,
+            parsedFile.Rows.Count);
+
+        ApplyStyles(worksheet);
+
+        return workbook;
+    }
+
+    public async Task ExportAsync(
+        XLWorkbook workbook,
+        string outputPath)
+    {
+        await Task.Run(() =>
+        {
+            workbook.SaveAs(outputPath);
+        });
+    }
+
+    private static IXLWorksheet CreateWorksheet(
+        XLWorkbook workbook)
+    {
+        return workbook.Worksheets
+            .Add("Transactions");
+    }
+
+    private static void InsertHeaders(
+        IXLWorksheet worksheet,
+        List<string> headers)
+    {
+        for (int column = 0;
+             column < headers.Count;
+             column++)
+        {
+            worksheet.Cell(1, column + 1)
+                .Value = headers[column];
+        }
+    }
+    
+    private static void InsertFooter(
+        IXLWorksheet worksheet, 
+        List<FooterItem> footerData)
+    {
+        int startRowPadding = 3;
+        int startRow;
+
+        IXLRow? lastRow =
+            worksheet.LastRowUsed();
+
+        if (lastRow is null)
+            return;
+
+        startRow =
+            lastRow.RowNumber() + startRowPadding;        
+
+        foreach((FooterItem item, int index) in footerData.Select((item, index) => (item, index)))
+        {
+            int currentRow = startRow + index;
+
+            worksheet.Cell(currentRow, 1)
+                .Value = item.Key;
+
+            worksheet.Cell(currentRow, 2)
+                .Value = item.Value;
+        }
+    }
+
+    private static void InsertRows(
+        IXLWorksheet worksheet,
+        List<List<string>> rows)
+    {
+        for (int row = 0;
+             row < rows.Count;
+             row++)
+        {
+            List<string> currentRow =
+                rows[row];
+
+            for (int column = 0;
+                 column < currentRow.Count;
+                 column++)
+            {
+                worksheet.Cell(
+                    row + 2,
+                    column + 1)
+                    .Value = currentRow[column];
+            }
+        }
+    }
+
+    private static void CreateTable(
+        IXLWorksheet worksheet,
+        int headersCount,
+        int rowsCount)
+    {
+        int startIndex = 1;
+        int lastColumn = headersCount;
+        int lastRow = rowsCount;
+
+        var tableRange = worksheet.Range(
+            startIndex,
+            startIndex,
+            lastRow,
+            lastColumn
+        );
+
+        IXLTable table =
+            tableRange.CreateTable();
+
+        table.Theme = XLTableTheme.TableStyleLight11;
+    }
+
+    private static void ApplyStyles(
+        IXLWorksheet worksheet)
+    {
+        worksheet.Columns()
+            .AdjustToContents();
+    }
+}
