e495436be34becff4148247e5d77e37d575f3b7e
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-05-10 18:07:30 -0600

    update: Services/Parsers/TransactionParser.cs
    Ignore the final two lines present in the file

--- Services/Parsers/TransactionParser.cs
+++ Services/Parsers/TransactionParser.cs
@@ -7,14 +7,19 @@ namespace Bank2Pdf.Services.Parsers
     class TransactionParser
     {
         private const int IGNORE_LINES_COUNT = 8;
+        private const int FOOTER_LINES_COUNT = 2;
 
         public List<List<string>> Parse(string[] lines)
         {
             return lines
                 .Skip(IGNORE_LINES_COUNT)
+                .Take(lines.Length
+                    - IGNORE_LINES_COUNT
+                    - FOOTER_LINES_COUNT)
                 .Select(line => line.Trim())
                 .Where(line => !string.IsNullOrWhiteSpace(line))
-                .Select(line => line.Split("|",
+                .Select(line => line.Split(
+                    "|",
                     StringSplitOptions.RemoveEmptyEntries))
                 .Select(parts => parts
                     .Select(x => x.Trim())
